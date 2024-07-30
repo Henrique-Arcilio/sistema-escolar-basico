56bb8aa85e0ca9a186cebb956f2db9bdf442f604
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-07-30 16:28:02 -0300

    Feat(ui): cria a interface gráfica do login
    O programa recebeu sua primeira janela, feita a partir do JavaFX.
    O arquivo fxml contendo a tela login:
    
    - src/main/resources/TelaLogin.fxml
    
    A tela login nesse presente momento não contém função real, apenas sua
    base visual feita.

--- src/main/java/com/arcilio/henrique/sistema_escolar/App.java
+++ src/main/java/com/arcilio/henrique/sistema_escolar/App.java
@@ -1,9 +1,11 @@
 package com.arcilio.henrique.sistema_escolar;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import com.arcilio.henrique.sistema_escolar.view.AppInterface;
+import javafx.application.Application;
 
+public class App {
+    public static void main(String[] args) {
+        AppInterface.iniciar(args);
     }
+
 }
--- src/test/java/com/arcilio/henrique/sistema_escolar/AppTest.java
+++ src/test/java/com/arcilio/henrique/sistema_escolar/AppTest.java
@@ -1,8 +1,7 @@
 package com.arcilio.henrique.sistema_escolar;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
